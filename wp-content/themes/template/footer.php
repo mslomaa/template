21baa1a3bba0908350ad3d82a00d335dddaaf132
mslomaa <slomkowskimateusz@gmail.com>
2021-11-04 22:27:32 +0100

    initial commit

---
+++ wp-content/themes/template/footer.php
@@ -0,0 +1,12 @@
+
+</div>
+<footer>
+        <h2>STOPKA</h2>
+    </footer>
+
+    <script src="./js/main.js"></script>
+
+    <?php wp_footer() ?>
+    
+</body>
+</html>
---
+++ wp-content/themes/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie rekrutacyjne</title>
+
+    <link href='<?php echo get_bloginfo('template_directory') ?>/style.css'/>
+    <?php wp_head() ?>
+</head>
+<body>
+    <nav>
+        <h2>MENU</h2>
+    </nav>
+    <div class="container">
+        <div class="container-header">
+            <h1 class="container-title">Nazwa strony</h1>
+        </div>
